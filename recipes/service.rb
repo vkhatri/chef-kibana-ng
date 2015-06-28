84bc08c46b0b0be1022095116a14c1c0ca7a63f2
Virender Khatri <vir.khatri@gmail.com>
2015-06-28 22:17:41 +0530

    added upstart service for debian platform

--- recipes/service.rb
+++ recipes/service.rb
@@ -19,17 +19,34 @@
 
 template '/etc/init.d/kibana' do
   source "init.#{node['platform_family']}.erb"
-  mode 0755
+  mode 0750
   variables(:daemon => node['kibana']['daemon'],
             :conf_file => node['kibana']['conf_file'],
             :log_file => node['kibana']['log_file'],
             :pid_file => node['kibana']['pid_file'],
             :elasticsearch_url => node['kibana']['config']['elasticsearch_url'],
             :user => node['kibana']['user'])
+  only_if { node['platform_family'] == 'rhel' }
+  notifies :restart, 'service[kibana]', :delayed if node['kibana']['notify_restart']
+end
+
+template '/etc/init/kibana.conf' do
+  mode 0750
+  cookbook node['kibana']['cookbook']
+  source 'upstart.erb'
+  variables(:daemon => node['kibana']['daemon'],
+            :install_dir => node['kibana']['install_dir'],
+            :conf_file => node['kibana']['conf_file'],
+            :log_file => node['kibana']['log_file'],
+            :user => node['kibana']['user'],
+            :group => node['kibana']['group']
+           )
+  only_if { node['platform_family'] == 'debian' }
   notifies :restart, 'service[kibana]', :delayed if node['kibana']['notify_restart']
 end
 
 service 'kibana' do
   supports :status => true, :reload => true, :restart => true
+  provider Chef::Provider::Service::Upstart if node['platform_family'] == 'debian'
   action [:enable, :start]
 end
