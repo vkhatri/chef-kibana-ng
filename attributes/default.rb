6d765fd9fd9fd628cef30d0fa7f0aba531397dbf
Virender Khatri <vir.khatri@gmail.com>
2015-07-06 09:35:23 +0530

    bump kibana version to v4.1.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,8 @@
-default['kibana']['version'] = '4.1.0'
+default['kibana']['version'] = '4.1.1'
 default['kibana']['tarball_url'] = "https://download.elasticsearch.org/kibana/kibana/kibana-#{node['kibana']['version']}-linux-x64.tar.gz"
 default['kibana']['tarball_checksum']['4.0.2'] = '4cc36e5c6ca7c495667319df75feda1facb7c43a3d9686841f07a2522adec294'
 default['kibana']['tarball_checksum']['4.1.0'] = '597e1b1e381b9a9ed9f8a66e115ec4d7a0258fa36c81fe74f1e91b651fcd567a'
+default['kibana']['tarball_checksum']['4.1.1'] = '6f42d25f337fd49f38e2af81b9ab6e0c987a199a8c0b2e1410d072f812cb4520'
 
 default['kibana']['user'] = 'kibana'
 default['kibana']['group'] = 'kibana'
